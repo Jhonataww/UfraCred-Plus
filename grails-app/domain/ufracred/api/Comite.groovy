e8ff91d61bb2f29c4ff7f3c0010199dd24138a09
Jhonata Williams <jhonatawwxx@gmail.com>
2023-09-27 20:18:52 -0300

    Feat: alteração na tabela de comite

--- grails-app/domain/ufracred/api/Comite.groovy
+++ grails-app/domain/ufracred/api/Comite.groovy
@@ -5,12 +5,14 @@ class Comite {
     Date aprovacao
     Integer excluido
     String parecer
+    Integer comite
 
     static belongsTo = [assessor: Assessor, proposta: Proposta]
 
     static constraints = {
         aprovacao nullable: false
-        excluido nullable: false
+        excluido nullable: true
         parecer nullable: false, maxSize: 255
+        comite nullable: false
     }
 }
